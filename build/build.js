e6a58f41f6d403e952788006bbbac5f0d5f3e4a4
alvin.hhh <alvin@jennifersoft.com>
2023-01-11 20:07:12 +0900

    a* 알고리즘 샘플 구현 완료

--- build/build.js
+++ build/build.js
@@ -61,33 +61,136 @@ var PolygonHelper = (function () {
     };
     return PolygonHelper;
 }());
-var numberOfShapesControl;
-function setup() {
-    console.log("🚀 - Setup initialized - P5 is running");
-    createCanvas(windowWidth, windowHeight);
-    rectMode(CENTER).noFill().frameRate(30);
-    numberOfShapesControl = createSlider(1, 30, 15, 1).position(10, 10).style("width", "100px");
+var cols = 25;
+var rows = 25;
+var grid = new Array(cols);
+var w;
+var h;
+var openSet = [];
+var closedSet = [];
+var start;
+var end;
+var path = [];
+var Spot = (function () {
+    function Spot(i, j) {
+        this.i = i;
+        this.j = j;
+        this.f = 0;
+        this.g = 0;
+        this.h = 0;
+        this.neighbors = [];
+        this.previous = undefined;
+    }
+    Spot.prototype.show = function (color) {
+        var _a = this, i = _a.i, j = _a.j;
+        fill(color);
+        noStroke();
+        rect(i * w, j * h, w - 1, h - 1);
+    };
+    Spot.prototype.addNeighbors = function (grid) {
+        var _a = this, i = _a.i, j = _a.j;
+        if (i < cols - 1)
+            this.neighbors.push(grid[i + 1][j]);
+        if (i > 0)
+            this.neighbors.push(grid[i - 1][j]);
+        if (j < rows - 1)
+            this.neighbors.push(grid[i][j + 1]);
+        if (j > 0)
+            this.neighbors.push(grid[i][j - 1]);
+    };
+    return Spot;
+}());
+function removeFromArray(arr, elt) {
+    for (var i = arr.length - 1; i >= 0; i--) {
+        if (arr[i] === elt) {
+            arr.splice(i, 1);
+        }
+    }
 }
-function windowResized() {
-    resizeCanvas(windowWidth, windowHeight);
+function heuristic(a, b) {
+    var d = abs(a.i - b.i) - abs(a.j - b.j);
+    return d;
+}
+function setup() {
+    createCanvas(400, 400);
+    w = width / cols;
+    h = height / rows;
+    for (var i = 0; i < cols; i++) {
+        grid[i] = new Array(rows);
+    }
+    for (var i = 0; i < cols; i++) {
+        for (var j = 0; j < rows; j++) {
+            grid[i][j] = new Spot(i, j);
+        }
+    }
+    for (var i = 0; i < cols; i++) {
+        for (var j = 0; j < rows; j++) {
+            grid[i][j].addNeighbors(grid);
+        }
+    }
+    start = grid[0][0];
+    end = grid[cols - 1][rows - 1];
+    openSet.push(start);
+    console.log(grid);
 }
 function draw() {
-    background(0);
-    translate(width / 2, height / 2);
-    var numberOfShapes = numberOfShapesControl.value();
-    var colours = ColorHelper.getColorsArray(numberOfShapes);
-    var speed = (frameCount / (numberOfShapes * 30)) * 2;
-    for (var i = 0; i < numberOfShapes; i++) {
-        push();
-        var lineWidth = 8;
-        var spin = speed * (numberOfShapes - i);
-        var numberOfSides = 3 + i;
-        var width_1 = 40 * i;
-        strokeWeight(lineWidth);
-        stroke(colours[i]);
-        rotate(spin);
-        PolygonHelper.draw(numberOfSides, width_1);
-        pop();
+    if (openSet.length > 0) {
+        var winner = 0;
+        for (var i = 0; i < openSet.length; i++) {
+            if (openSet[i].f < openSet[winner].f) {
+                winner = i;
+            }
+        }
+        var current = openSet[winner];
+        if (current === end) {
+            noLoop();
+            console.log('DONE!');
+        }
+        removeFromArray(openSet, current);
+        closedSet.push(current);
+        var neighbors = current.neighbors;
+        for (var i = 0; i < neighbors.length; i++) {
+            var neighbor = neighbors[i];
+            if (closedSet.indexOf(neighbor) === -1) {
+                var tempG = current.g + 1;
+                if (openSet.indexOf(neighbor) !== -1) {
+                    if (tempG < neighbor.g) {
+                        neighbor.g = tempG;
+                    }
+                }
+                else {
+                    neighbor.g = tempG;
+                    openSet.push(neighbor);
+                }
+                neighbor.h = heuristic(neighbor, end);
+                neighbor.f = neighbor.g + neighbor.h;
+                neighbor.previous = current;
+            }
+        }
+        background(0);
+        for (var i = 0; i < cols; i++) {
+            for (var j = 0; j < rows; j++) {
+                grid[i][j].show(color(255));
+            }
+        }
+        for (var i = 0; i < closedSet.length; i++) {
+            closedSet[i].show(color(255, 0, 0));
+        }
+        for (var i = 0; i < openSet.length; i++) {
+            openSet[i].show(color(0, 255, 0));
+        }
+        path = [];
+        var temp = current;
+        path.push(temp);
+        while (temp.previous) {
+            path.push(temp.previous);
+            temp = temp.previous;
+        }
+        for (var i = 0; i < path.length; i++) {
+            path[i].show(color(0, 0, 255));
+        }
+    }
+    else {
     }
 }
 //# sourceMappingURL=build.js.map
--- sketch/sketch.ts
+++ sketch/sketch.ts
@@ -1,48 +1,163 @@
 // GLOBAL VARS & TYPES
-let numberOfShapesControl: p5.Element
+const cols = 25;
+const rows = 25;
+const grid = new Array(cols);
+let w: number;
+let h: number;
+
+const openSet: Array<Spot> = [];
+const closedSet: Array<Spot> = [];
+let start: Spot;
+let end: Spot;
+let path: Array<Spot> = [];
+
+class Spot {
+    i: number;
+    j: number;
+    f: number;
+    g: number;
+    h: number;
+    neighbors: Spot[];
+    previous: Spot;
+
+    constructor(i: number, j: number) {
+        this.i = i;
+        this.j = j;
+        this.f = 0;
+        this.g = 0;
+        this.h = 0;
+        this.neighbors = [];
+        this.previous = undefined;
+    }
+
+    show(color: p5.Color): void {
+        const { i, j } = this;
+        fill(color);
+        noStroke();
+        rect(i * w, j * h, w - 1, h - 1);
+    }
+
+    addNeighbors(grid: Spot[][]) {
+        const { i, j } = this;
+        if (i < cols - 1) this.neighbors.push(grid[i + 1][j]);
+        if (i > 0) this.neighbors.push(grid[i - 1][j]);
+        if (j < rows - 1) this.neighbors.push(grid[i][j + 1]);
+        if (j > 0) this.neighbors.push(grid[i][j - 1]);
+    }
+}
+
+function removeFromArray(arr: Spot[], elt: Spot) {
+    for (let i = arr.length - 1; i >= 0; i--) {
+        if (arr[i] === elt) {
+            arr.splice(i, 1);
+        }
+    }
+}
+
+function heuristic(a: Spot, b: Spot) {
+    // const d = dist(a.i, a.j, b.i, b.j);
+    const d = abs(a.i - b.i) - abs(a.j - b.j);
+    return d;
+}
 
 // P5 WILL AUTOMATICALLY USE GLOBAL MODE IF A DRAW() FUNCTION IS DEFINED
 function setup() {
-    console.log('🚀 - Setup initialized - P5 is running')
+    createCanvas(400, 400);
 
-    createCanvas(windowWidth, windowHeight)
-    rectMode(CENTER).noFill().frameRate(30)
-    // NUMBER OF SHAPES SLIDER
-    numberOfShapesControl = createSlider(1, 30, 15, 1)
-        .position(10, 10)
-        .style('width', '100px')
-}
+    w = width / cols;
+    h = height / rows;
 
-// p5 WILL AUTO RUN THIS FUNCTION IF THE BROWSER WINDOW SIZE CHANGES
-function windowResized() {
-    resizeCanvas(windowWidth, windowHeight)
+    for (let i = 0; i < cols; i++) {
+        grid[i] = new Array(rows);
+    }
+
+    for (let i = 0; i < cols; i++) {
+        for (let j = 0; j < rows; j++) {
+            grid[i][j] = new Spot(i, j);
+        }
+    }
+
+    for (let i = 0; i < cols; i++) {
+        for (let j = 0; j < rows; j++) {
+            grid[i][j].addNeighbors(grid);
+        }
+    }
+
+    start = grid[0][0];
+    end = grid[cols - 1][rows - 1];
+    openSet.push(start);
+
+    console.log(grid);
 }
 
 // p5 WILL HANDLE REQUESTING ANIMATION FRAMES FROM THE BROWSER AND WIL RUN DRAW() EACH ANIMATION FROME
 function draw() {
-    // CLEAR BACKGROUND
-    background(0)
+    if (openSet.length > 0) {
+        let winner = 0;
+        for (let i = 0; i < openSet.length; i++) {
+            if (openSet[i].f < openSet[winner].f) {
+                winner = i;
+            }
+        }
 
-    // CENTER OF SCREEN
-    translate(width / 2, height / 2)
+        const current = openSet[winner];
+        if (current === end) {
+            noLoop();
+            console.log('DONE!');
+        }
 
-    const numberOfShapes = <number>numberOfShapesControl.value()
-    const colours = ColorHelper.getColorsArray(numberOfShapes)
+        removeFromArray(openSet, current);
+        closedSet.push(current);
 
-    // CONSISTENT SPEED REGARDLESS OF FRAMERATE
-    const speed = (frameCount / (numberOfShapes * 30)) * 2
+        const neighbors = current.neighbors;
+        for (let i = 0; i < neighbors.length; i++) {
+            const neighbor = neighbors[i];
 
-    // DRAW ALL SHAPES
-    for (var i = 0; i < numberOfShapes; i++) {
-        push()
-        const lineWidth = 8
-        const spin = speed * (numberOfShapes - i)
-        const numberOfSides = 3 + i
-        const width = 40 * i
-        strokeWeight(lineWidth)
-        stroke(colours[i])
-        rotate(spin)
-        PolygonHelper.draw(numberOfSides, width)
-        pop()
+            if (closedSet.indexOf(neighbor) === -1) {
+                const tempG = current.g + 1;
+
+                if (openSet.indexOf(neighbor) !== -1) {
+                    if (tempG < neighbor.g) {
+                        neighbor.g = tempG;
+                    }
+                } else {
+                    neighbor.g = tempG;
+                    openSet.push(neighbor);
+                }
+
+                neighbor.h = heuristic(neighbor, end);
+                neighbor.f = neighbor.g + neighbor.h;
+                neighbor.previous = current;
+            }
+        }
+        background(0);
+
+        for (let i = 0; i < cols; i++) {
+            for (let j = 0; j < rows; j++) {
+                grid[i][j].show(color(255));
+            }
+        }
+
+        for (let i = 0; i < closedSet.length; i++) {
+            closedSet[i].show(color(255, 0, 0));
+        }
+
+        for (let i = 0; i < openSet.length; i++) {
+            openSet[i].show(color(0, 255, 0));
+        }
+
+        path = [];
+        let temp = current;
+        path.push(temp);
+        while (temp.previous) {
+            path.push(temp.previous);
+            temp = temp.previous;
+        }
+
+        for (let i = 0; i < path.length; i++) {
+            path[i].show(color(0, 0, 255));
+        }
+    } else {
+        // TODO: no solution
     }
 }
