2a79a76d472b8bc6a5154b90a81178637550324c
alvin.hhh <alvin@jennifersoft.com>
2023-02-01 20:25:33 +0900

    Quadtree 샘플 추가 중... 31분

---
+++ sketch/quadtree.ts
@@ -0,0 +1,108 @@
+class Point {
+    x: number;
+    y: number;
+
+    constructor(x: number, y: number) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+class Rectangle {
+    x: number;
+    y: number;
+    w: number;
+    h: number;
+
+    constructor(x: number, y: number, w: number, h: number) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+    }
+
+    contains(point: Point) {
+        return (
+            point.x > this.x - this.w &&
+            point.x < this.x + this.w &&
+            point.y > this.y - this.h &&
+            point.y < this.y + this.h
+        );
+    }
+}
+
+class QuadTree {
+    boundary: Rectangle;
+    capacity: number;
+    points: Point[];
+    northwest: QuadTree;
+    northeast: QuadTree;
+    southwest: QuadTree;
+    southeast: QuadTree;
+    divided: boolean;
+
+    constructor(boundary: Rectangle, n: number) {
+        this.boundary = boundary;
+        this.capacity = n;
+        this.points = [];
+        this.divided = false;
+    }
+
+    subdivide() {
+        const { x, y, w, h } = this.boundary;
+        const capacity = this.capacity;
+
+        let nw = new Rectangle(x - w / 2, y - h / 2, w / 2, h / 2);
+        this.northwest = new QuadTree(nw, capacity);
+
+        let ne = new Rectangle(x + w / 2, y - h / 2, w / 2, h / 2);
+        this.northeast = new QuadTree(ne, capacity);
+
+        let sw = new Rectangle(x - w / 2, y + h / 2, w / 2, h / 2);
+        this.southwest = new QuadTree(sw, capacity);
+
+        let se = new Rectangle(x + w / 2, y + h / 2, w / 2, h / 2);
+        this.southeast = new QuadTree(se, capacity);
+
+        this.divided = true;
+    }
+
+    insert(point: Point) {
+        if (!this.boundary.contains(point)) {
+            return;
+        }
+
+        if (this.points.length < this.capacity) {
+            this.points.push(point);
+        } else {
+            if (!this.divided) {
+                this.subdivide();
+            }
+            this.northeast.insert(point);
+            this.northwest.insert(point);
+            this.southeast.insert(point);
+            this.southwest.insert(point);
+        }
+    }
+
+    show() {
+        const { x, y, w, h } = this.boundary;
+        stroke(255);
+        strokeWeight(1);
+        noFill();
+        rectMode(CENTER);
+        rect(x, y, w * 2, h * 2);
+
+        if (this.divided) {
+            this.northwest.show();
+            this.northeast.show();
+            this.southwest.show();
+            this.southeast.show();
+        }
+
+        for (let p of this.points) {
+            strokeWeight(4);
+            point(p.x, p.y);
+        }
+    }
+}
