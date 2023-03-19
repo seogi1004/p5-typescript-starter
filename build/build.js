81cdbaef6772fa4e8730f0d16e7b7cc38d5aaa83
alvin <alvin@jennifersoft.com>
2023-03-19 12:00:44 +0900

    steering behaviors 샘플 추가 #2

--- build/build.js
+++ build/build.js
@@ -1,8 +1,28 @@
+var vehicle;
+var food = [];
 function setup() {
-    createCanvas(windowWidth, windowHeight);
+    createCanvas(640, 360);
+    vehicle = new Vehicle(width / 2, height / 2);
+    for (var i = 0; i < 10; i++) {
+        var x = random(width);
+        var y = random(height);
+        food.push(createVector(x, y));
+    }
 }
 function draw() {
-    background(0);
+    background(51);
+    var target = createVector(mouseX, mouseY);
+    fill(127);
+    stroke(200);
+    strokeWeight(2);
+    ellipse(target.x, target.y, 48, 48);
+    for (var i = 0; i < food.length; i++) {
+        fill(0, 255, 0);
+        ellipse(food[i].x, food[i].y, 8, 8);
+    }
+    vehicle.seek(target);
+    vehicle.update();
+    vehicle.display();
 }
 var Vehicle = (function () {
     function Vehicle(x, y) {
--- sketch/sketch.ts
+++ sketch/sketch.ts
@@ -1,9 +1,33 @@
+let vehicle: Vehicle;
+const food: p5.Vector[] = [];
+
 function setup() {
-    createCanvas(windowWidth, windowHeight)
+    createCanvas(640, 360)
+    vehicle = new Vehicle(width / 2, height / 2);
+    for (let i = 0; i < 10; i++) {
+        const x = random(width);
+        const y = random(height);
+        food.push(createVector(x, y));
+    }
 }
 
 function draw() {
-    // CLEAR BACKGROUND
-    background(0)
+    background(51);
+
+    const target = createVector(mouseX, mouseY);
+
+    fill(127);
+    stroke(200);
+    strokeWeight(2);
+    ellipse(target.x, target.y, 48, 48);
+
+    for (let i = 0; i < food.length; i++) {
+        fill(0, 255, 0)
+        ellipse(food[i].x, food[i].y, 8, 8);
+    }
+
+    vehicle.seek(target);
+    vehicle.update();
+    vehicle.display();
 
 }
